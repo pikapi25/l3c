0a86d472475597c4801f192795c0b9e8e413ebf2
yunzhil3 <yunzhil3@illinois.edu>
2023-12-06 00:23:12 -0600

    piano controlled by numlock. try to write a syscall

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -2,14 +2,15 @@
 #include "lib.h"
 #include "i8259.h"
 #include "terminal.h"
-// #include "speaker.h"
+#include "speaker.h"
+#include "gensound.h"
 
 //flags of modifier keys
 uint8_t caps  = 0;
 uint8_t ctrl  = 0;
 uint8_t shift = 0;
 uint8_t alt = 0;	//added in cp2 but not used
-// uint8_t numl  = 0;
+uint8_t numl  = 0;
 
 //scan code table1
 //If there is no correspoding ascii code in the scan code set, output '\0'
@@ -117,9 +118,9 @@ void keyboard_handler(void) {
 		case CAPS_LOCK_PRESSED:		
 			caps = !caps;	
 			break;
-		// case NUM_LOCK_PRESSED:		
-		// 	numl = !numl;	
-		// 	break;
+		case NUM_LOCK_PRESSED:		
+			numl = !numl;	
+			break;
 		//--------------------checkpoint5-----------------------
 		/* terminal switch*/
 		case F1_KEY:
@@ -141,11 +142,12 @@ void keyboard_handler(void) {
 		default:
             //get corresponding ascii for letters and numbers
             //invalid scan code, break
-			//temp handler in checkpoint1 and checkpoint2
-			// if (scan_code >= 0x80) {//release
-			// 	speaker_stop();
-			// 	break;
-			// }
+
+			//release piano key
+			if (numl && scan_code >= 0x80) {
+				speaker_stop();
+				break;
+			}
 			if (scan_code >= SCAN_CODE_PRESS) break;
 			//if shift and caps are both pressed
 			else if (shift && caps) {
@@ -171,48 +173,10 @@ void keyboard_handler(void) {
             // putc(ascii);
 			// break;
 			//--------------------checkpoint2-----------------------
-			// if (numl) {
-			// 	switch (ascii) {
-			// 		case 'z':	case 'Z':	speaker_play(FREQ_C3);	break;
-			// 		case 's':	case 'S':	speaker_play(FREQ_CS3);	break;
-			// 		case 'x':	case 'X':	speaker_play(FREQ_D3);	break;
-			// 		case 'd':	case 'D':	speaker_play(FREQ_DS3);	break;
-			// 		case 'c':	case 'C':	speaker_play(FREQ_E3);	break;
-			// 		case 'v':	case 'V':	speaker_play(FREQ_F3);	break;
-			// 		case 'g':	case 'G':	speaker_play(FREQ_FS3);	break;
-			// 		case 'b':	case 'B':	speaker_play(FREQ_G3);	break;
-			// 		case 'h':	case 'H':	speaker_play(FREQ_GS3);	break;
-			// 		case 'n':	case 'N':	speaker_play(FREQ_A3);	break;
-			// 		case 'j':	case 'J':	speaker_play(FREQ_AS3);	break;
-			// 		case 'm':	case 'M':	speaker_play(FREQ_B3);	break;
-			// 		case ',':	case '<':	speaker_play(FREQ_C4);	break;
-			// 		case 'l':	case 'L':	speaker_play(FREQ_CS4);	break;
-			// 		case '.':	case '>':	speaker_play(FREQ_D4);	break;
-			// 		case ';':	case ':':	speaker_play(FREQ_DS4);	break;
-			// 		case '/':	case '?':	speaker_play(FREQ_E4);	break;
-			// 		case 'q':	case 'Q':	speaker_play(FREQ_C4);	break;
-			// 		case '2':				speaker_play(FREQ_CS4);	break;
-			// 		case 'w':	case 'W':	speaker_play(FREQ_D4);	break;
-			// 		case '3':				speaker_play(FREQ_DS4);	break;
-			// 		case 'e':	case 'E':	speaker_play(FREQ_E4);	break;
-			// 		case 'r':	case 'R':	speaker_play(FREQ_F4);	break;
-			// 		case '5':				speaker_play(FREQ_FS4);	break;
-			// 		case 't':	case 'T':	speaker_play(FREQ_G4);	break;
-			// 		case '6':				speaker_play(FREQ_GS4);	break;
-			// 		case 'y':	case 'Y':	speaker_play(FREQ_A4);	break;
-			// 		case '7':				speaker_play(FREQ_AS4);	break;
-			// 		case 'u':	case 'U':	speaker_play(FREQ_B4);	break;
-			// 		case 'i':	case 'I':	speaker_play(FREQ_C5);	break;
-			// 		case '9':				speaker_play(FREQ_CS5);	break;
-			// 		case 'o':	case 'O':	speaker_play(FREQ_D5);	break;
-			// 		case '0':				speaker_play(FREQ_DS5);	break;
-			// 		case 'p':	case 'P':	speaker_play(FREQ_E5);	break;
-			// 		case '[':	case '{':	speaker_play(FREQ_F5);	break;
-			// 		case '=':	case '+':	speaker_play(FREQ_FS5);	break;
-			// 		case ']':	case '}':	speaker_play(FREQ_G5);	break;
-			// 	}
-			// 	break;
-			// }
+			if (numl) {
+				piano(ascii);
+				break;
+			}
 			//ctrl+l/L: clean the screen 
 			if (ctrl && (ascii == 'l' || ascii == 'L')) {
 				clear_redraw();						
@@ -265,3 +229,39 @@ void keyboard_handler(void) {
 	sti();
 }
 
+void piano(uint8_t ascii){
+	switch (ascii) {
+		case '1':	case '!':	
+			speaker_play(C3);	
+			break;
+		case '2':	case '@':	
+			speaker_play(D3);	
+			break;
+		case '3':	case '#':	
+			speaker_play(E3);	
+			break;
+		case '4':	case '$':	
+			speaker_play(F3);	
+			break;
+		case '5':	case '%':	
+			speaker_play(G3);	
+			break;
+		case '6':	case '^':	
+			speaker_play(A3);	
+			break;
+		case '7':	case '&':	
+			speaker_play(B3);	
+			break;
+		case '8':	case '*':	
+			speaker_play(C4);	
+			break;
+		case '9':	case '(':	
+			speaker_play(D4);	
+			break;
+		case '0':	case ')':	
+			speaker_play(E4);	
+			break;
+		default:
+			return;
+	}
+}
--- student-distrib/speaker.c
+++ student-distrib/speaker.c
@@ -2,43 +2,28 @@
 #include "scheduler.h"
 #include "speaker.h"
 
-// code inspired by https://wiki.osdev.org/PC_Speaker
-
-/*
- * speaker_play
- * DESCRIPTION: start playing sound at a given frequency
- * INPUTS: freq
- * OUTPUTS: none
- * RETURN VALUE: none
- * SIDE EFFECTS: preempt Channel 2 of PIT, 
- * 				 changing its mode and frequency
- */
-void speaker_play(unsigned int freq)
+//reference: https://wiki.osdev.org/PC_Speaker
+void speaker_play(uint32_t nFrequence)
 {
-	// tweak PIT to channel 2 & set frequency
-	uint32_t temp, div = PIT_FREQ_MAX / freq;
-	outb(PIT_MODE_CH2, PIT_IO_PORT);
-	outb((uint8_t)div, PIT_DATA_PORT_CH2);
-	outb((uint8_t)(div >> 8), PIT_DATA_PORT_CH2);
-
-	// turn on speaker (set bits 0 and 1 of port 0x61)
-	temp = inb(SPK_DATA_PORT);
-	if (temp != (temp | 3)) {
-		outb(temp | 3, SPK_DATA_PORT);
-	}
+	uint32_t Div;
+ 	uint8_t tmp;
+ 
+        //Set the PIT to the desired frequency
+ 	Div = 1193180 / nFrequence;
+ 	outb(0xb6, 0x43);
+ 	outb((uint8_t) (Div), 0x42);
+ 	outb((uint8_t) (Div >> 8), 0x42);
+ 
+        //And play the sound using the PC speaker
+ 	tmp = inb(0x61);
+  	if (tmp != (tmp | 3)) {
+ 		outb(tmp | 3, 0x61);
+ 	}
 }
 
-/*
- * speaker_stop
- * DESCRIPTION: stop playing sound
- * INPUTS: none
- * OUTPUTS: none
- * RETURN VALUE: none
- * SIDE EFFECTS: reset PIT to channel 0
- */
 void speaker_stop(void)
 {
 	// turn off speaker (clear bits 0 and 1 of port 0x61)
-	uint8_t temp = inb(SPK_DATA_PORT) & 0xFC;
-	outb(temp, SPK_DATA_PORT);
+	uint8_t temp = inb(0x61) & 0xFC;
+	outb(temp, 0x61);
 }
